32ea9deaa8b53dfaa5c6850d5b80e78bde87bd84
Víctor Bolaños Solís <viktorb.132@gmail.com>
2022-03-07 21:27:49 -0600

    Creacion de reducer y coneccion a usuarios

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
 
+const store = createStore(
+  reducers,  //todos los reducers
+  {}
+)
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
